9eb0caa547b67e78460bfdc47a1d782fba39697e
enzoop4 <enzoordp@gmail.com>
2024-10-10 16:05:47 -0300

    Tarea 3 ejercicios java

---
+++ ejercicios.js
@@ -0,0 +1,150 @@
+// Operadores de comparación
+// 1. Comprueba si 10 es mayor o igual que 9 e imprime el resultado en consola.
+function compareTenAndNine(numero) {
+    const numero = (10 >= 9)
+    console.log('El numero 10 es mayor o igual a 9'); resultado
+}
+
+
+// 2. Comprueba si 0 es igual a 0 e imprime el resultado en consola.
+function compareZeroAndZero() {
+    const ceros = (0 === 0)
+    console.log('Los numeros ceros son distintos'); resultado
+}
+
+// 3. Comprueba si 7 es mayor que 8 y menor que 10, e imprime el resultado en consola.
+function compareSeven(numero1){
+    const numero1 = (7 < 10)
+    const numero2 = (8 < 10)
+    console.log("El numero 7 es menor que 10; "), numero1
+    console.log("El numero 7 es menor que 10; "), numero2
+}
+
+
+// Operadores lógicos
+// 4. Verifica si puedes comprar un producto de 1500 con un descuento del 25% teniendo 1150€ e imprime si puedes o no.
+function canBuyProduct(producto){
+    const producto = (1500 * 0.25)
+    console.log(producto)
+}
+
+// Variables
+// 5. Crea una variable llamada `mensaje` y asígnale el valor "Hola JavaScript", luego imprime la variable en consola.
+function createMessageVariable(mensaje) {
+    const mensaje =("Hola JavaScript")
+    console.log = (mensaje )
+}
+
+// 6. Crea una variable llamada `resultado` y asígnale la suma de 2 y 3, luego imprime la variable en consola.
+function createSumVariable(resultado) {
+    const resultado = (2+3)
+    console.log(resultado)
+}
+
+// 7. Crea una constante llamada `IS_DISABLED` y asígnale el valor booleano `true`, luego imprime la constante en consola.
+function createDisabledConstant(IS_DISABLED) {
+    const IS_DISABLED = (true)
+    console.log(IS_DISABLED)
+}
+
+// Null y Undefined
+// 8. Crea una variable con `let` llamada `capacidad` y asígnale un valor `null`, luego imprime la variable en consola.
+function createNullVariable() {
+    let capacidad = ('null')
+    console.log(capacidad)
+}
+
+// 9. Crea una variable con `let` llamada `dinero` y asígnale un valor `undefined`, luego imprime la variable en consola.
+function createUndefinedVariable() {
+    let dinero = (undefined)
+    console.leg(dinero)
+}
+
+// typeof
+// 10. Escribe un código para ver el tipo de una variable llamada `userName` e imprime el tipo de dato en consola.
+function checkUserNameType(userName) {
+    const userName = (enzoop)
+    console.log(userName)
+}
+
+// 11. Asegúrate de que `dogId` es una cadena de texto y luego imprime el tipo de dato en consola.
+function checkDogIdIsString(dogId) {
+    const dogId = (24)
+    console.log = (dogId)
+}
+
+// console.log()
+// 12. Imprime un mensaje en la consola utilizando `console.log()`.
+function logMessage(mensaje) {
+    console.log(logMessage)
+}
+
+// 13. Utiliza `console.log()` para imprimir el valor de la variable `edad` que tiene el valor 30.
+function logAge() {
+    const edad = 30
+    console.log = (edad)
+}
+
+// 14. Muestra el mensaje 'La versión de JavaScript es' seguido de la variable `version` con el valor 2024 usando `console.log()`.
+function logVersion(version) {
+    const version = (2024)
+    console.log('La version de JavaScript' + version)
+}
+
+// Exportar todas las funciones para poder usarlas en las pruebas
+module.exports = {
+    compareTenAndNine,
+    compareZeroAndZero,
+    compareSeven,
+    canBuyProduct,
+    createMessageVariable,
+    createSumVariable,
+    createDisabledConstant,
+    createNullVariable,
+    createUndefinedVariable,
+    checkUserNameType,
+    checkDogIdIsString,
+    logMessage,
+    logAge,
+    logVersion,
+};{
+const compareTenAndNine = (10 > 9)
+console.log = ('Diez es mayor que nueve' + compareTenAndNine)
+}
+{
+    const compareZeroAndZero = (0 === 0)
+    console.log = ('Cero y cero son iguales'+ compareZeroAndZero)
+}{
+    const compareSeven = (7 === 7)
+    console.log('Siete y siete son iguales' + compareSeven)
+}{
+    const canBuyProduct = (Harina)
+    console.log('Puedo comprar un producto'+ canBuyProduct)
+}{
+    const createMessageVariable = (variable)
+    console.log(createMessageVariable)
+}{
+    const createDisabledConstant = (constant)
+    console.log(createDisabledConstant)
+}{
+    const createNullVariable = (variable1)
+    console.log(createNullVariable)
+}{
+    const createUndefinedVariable = (variable2)
+    console.log(createUndefinedVariable)
+}{
+    const checkUserNameType = (enzoop1)
+    console.log(checkUserNameType)
+}{
+    const checkDogIdIsString = (dogger)
+    console.log(checkDogIdIsString)
+}{
+    const logMessage = ("Hola a Todos")
+    console.log(logMessage)
+}{
+    const logAge = (2024)
+    console.log(logAge)
+}{
+    const logVersion = (version1)
+    console.log(logVersion)
+}
